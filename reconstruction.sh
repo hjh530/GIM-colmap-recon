4ffea65eb873ecd0017f88bd66c0a8f8519a24a4
Junhui He <83948418+hjh530@users.noreply.github.com>
2026-04-22 13:47:58 +0800

    Add stop_after_db argument to reconstruction script

--- reconstruction.sh
+++ reconstruction.sh
@@ -2,6 +2,6 @@
 scene_name=$1
 version=$2
 
-python reconstruction.py --scene_name ${scene_name} --version ${version}
+python reconstruction.py --scene_name ${scene_name} --version ${version} --stop_after_db=args.stop_after_db
 
 
