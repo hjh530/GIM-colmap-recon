00c9185deb16c134c81e8a758bf4e01f51d56750
Junhui He <83948418+hjh530@users.noreply.github.com>
2026-04-22 11:29:39 +0800

    Update script to call reconstruction.py instead of reconstruction1.py

---
+++ reconstruction.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+scene_name=$1
+version=$2
+
+python reconstruction.py --scene_name ${scene_name} --version ${version}
+
+
--- reconstruction2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-scene_name=$1
-version=$2
-
-python reconstruction1.py --scene_name ${scene_name} --version ${version}
-
-
